a3444f1a5a746af14fdfca4c59a1c56eccb2ac8f
Anothor <anothordev@gmail.com>
2024-05-30 11:00:20 +0700

    Update slugify.js
    Remove trailing -

--- slugify.js
+++ slugify.js
@@ -6,7 +6,8 @@ function slugify(text) {
     .trim()                                  // Remove whitespace from both sides of a string (optional)
     .replace(/\s+/g, '-')            // Replace spaces with -
     .replace(/[^\w\-]+/g, '')     // Remove all non-word chars
-    .replace(/\-\-+/g, '-');        // Replace multiple - with single -
+    .replace(/\-\-+/g, '-')        // Replace multiple - with single -
+    .replace(/\-$/g, '');         // Remove trailing -
 }
 
 
